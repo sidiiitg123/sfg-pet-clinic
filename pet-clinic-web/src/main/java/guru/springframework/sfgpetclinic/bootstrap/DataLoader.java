e209bfb0a224215c7360492fa58e16fa504090b5
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2020-12-14 20:09:06 +0530

    Closing #22

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,4 +1,42 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
-public class DataLoader {
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.OwnerMapService;
+import guru.springframework.sfgpetclinic.services.map.VetMapService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Owner o1= new Owner();
+        o1.setFirstName("sudhanshu");
+        o1.setId(1l);
+        o1.setLastName("upadhyay");
+
+        ownerService.save(o1);
+
+        Owner o2= new Owner();
+        o2.setFirstName("shivani");
+        o2.setId(2l);
+        o2.setLastName("tripathi");
+
+        ownerService.save(o2);
+
+        System.out.println("sab thik se ho gaya no error");
+
+    }
 }
