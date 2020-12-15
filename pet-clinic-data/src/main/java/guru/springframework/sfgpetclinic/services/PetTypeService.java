22c64478f76c832874f2fcd9680ed6a486fb163c
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2020-12-16 01:10:57 +0530

    Addded properties Closing #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -1,11 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
-import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 
-import java.util.Set;
-
-public interface PetService extends CrudService<Pet,Long> {
-
+public interface PetTypeService  extends CrudService<PetType,Long> {
 }
